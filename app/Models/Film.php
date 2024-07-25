6db463842f8bbb5a494777192529bf00590ef1f9
fabyoo7 <arafatkurnia11@gmail.com>
2024-07-25 14:00:01 +0700

    restapi

---
+++ app/Models/Film.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class , 'id_kategori');
+    }
+
+    public function genre()
+    {
+        return $this->belongsToMany(Genre::class ,'genre_film','id_film','id_genre');
+    }
+
+    public function actor()
+    {
+        return $this->belongsToMany(Actor::class ,'genre_film','id_film','id_actor');
+    }
+}
