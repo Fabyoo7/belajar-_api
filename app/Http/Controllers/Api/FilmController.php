ac7da4f6810fca546c91d9f7908a128b397d1d3a
fabyoo7 <arafatkurnia11@gmail.com>
2024-07-26 14:35:05 +0700

    baruuu

--- app/Http/Controllers/Api/FilmController.php
+++ app/Http/Controllers/Api/FilmController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\Controller;
 use App\Models\Film;
 use Illuminate\Http\Request;
 use Storage;
