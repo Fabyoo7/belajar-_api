5a6d4d8576b91ca6a37e591cea65cced14f596b1
fabyoo7 <arafatkurnia11@gmail.com>
2024-08-08 10:30:31 +0700

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-// })->middleware('auth:sanctum');
+})->middleware('auth:sanctum');
 
 // Route::middleware((['auth:sanctum']))->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::resource('kategori', KategoriController::class);
     Route::resource('genre', GenreController::class);
     Route::resource('aktor', AktorController::class);
-});
+// });
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
